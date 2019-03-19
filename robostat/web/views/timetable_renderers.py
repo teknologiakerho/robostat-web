8c867e23f7ca401c23bef1aca4c49d9d4b09ed1d
tl <tl@cat.pm>
2019-03-19 11:41:52 +0200

    aikataulu: tanssi lisätty

--- robostat/web/views/timetable_renderers.py
+++ robostat/web/views/timetable_renderers.py
@@ -2,6 +2,7 @@ import flask
 from robostat.ruleset import Ruleset
 from robostat.rulesets.xsumo import XSumoRuleset
 from robostat.rulesets.rescue import RescueRuleset
+from robostat.rulesets.tanssi import DanceInterviewRuleset, DancePerformanceRuleset
 from robostat.rulesets.haastattelu import HaastatteluRuleset
 from robostat.web.views.timetable import event_renderer
 
@@ -17,6 +18,11 @@ def render_xsumo_event(event):
 def render_recue_event(event):
     return flask.render_template("timetable/event-rescue.html", event=event)
 
+@event_renderer.of(DancePerformanceRuleset)
+def render_dance_event(event):
+    return flask.render_template("timetable/event-dance.html", event=event)
+
 @event_renderer.of(HaastatteluRuleset)
+@event_renderer.of(DanceInterviewRuleset)
 def render_haastattelu_event(event):
     return flask.render_template("timetable/event-haastattelu.html", event=event)
