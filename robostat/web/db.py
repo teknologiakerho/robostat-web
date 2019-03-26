cc600c904113bb9c7135dc344b3645d054466116
tl <tl@cat.pm>
2019-03-26 22:32:42 +0200

    db: judge key nullableksi, server defaultit

--- robostat/web/db.py
+++ robostat/web/db.py
@@ -3,9 +3,9 @@ from robostat.db import *
 from robostat.util import lazy
 from robostat.web.util import decode_score
 
-Team.school = sa.Column(sa.Text, default="", nullable=False)
-Team.desc = sa.Column(sa.Text, default="", nullable=False)
+Team.school = sa.Column(sa.Text, default="", server_default="", nullable=False)
+Team.desc = sa.Column(sa.Text, default="", server_default="", nullable=False)
 
-Judge.key = sa.Column(sa.Text, unique=True, nullable=True)
+Judge.key = sa.Column(sa.Text, unique=True)
 
 Score.score_obj = lazy(decode_score)
