332fea63213f74bdba1f9ad07728097dcffa07d8
tl <tl@cat.pm>
2019-04-18 01:17:26 +0300

    app: create_app() lisätty
    Nopeaa testausta tai yksinkertaisia ajoja varten.
    Yleensä RobostatWeb instanssin luominen itse on parempi idea.

--- robostat/web/app.py
+++ robostat/web/app.py
@@ -10,13 +10,15 @@ from robostat.web.login import UnauthorizedError
 
 class RobostatWeb(flask.Flask):
 
-    def __init__(self, import_name, tournament=None, admin_password=None, **kwargs):
+    def __init__(self, import_name, tournament=None, **kwargs):
         super().__init__(import_name, **kwargs)
         self.register_blueprint(res)
-        self.register_blueprint(self.create_login_blueprint(admin_password), url_prefix="/auth")
         self.register_error_handler(UnauthorizedError, self.handle_unauth)
         self.db = None
         self.tournament = tournament or robostat.default_tournament
+
+    def setup_login(self, admin_password=None):
+        self.register_blueprint(self.create_login_blueprint(admin_password), url_prefix="/auth")
 
     def create_login_blueprint(self, admin_password):
         from robostat.web.views.login import LoginView
@@ -39,3 +41,79 @@ class RobostatWeb(flask.Flask):
             flask.url_for(redir),
             flask.request.url
         ))
+
+def create_app(config_pyfile=None, config_envvar="ROBOSTAT_CONFIG",
+        admin_password=None, tournament=None,
+        init_file=None, init_envvar="ROBOSTAT_INIT",
+        db_url=None, db_envvar="ROBOSTAT_DB",
+        import_name=__name__):
+
+    import os
+
+    app = RobostatWeb(import_name)
+
+    config_pyfile = config_pyfile or os.environ.get(config_envvar, None)
+
+    if config_pyfile is not None:
+        app.config.from_pyfile(config_pyfile)
+
+    init = init_file or os.environ.get(init_envvar, None) or app.config.get("ROBOSTAT_INIT", None)
+
+    if init is None:
+        raise ValueError("No init file given")
+
+    if tournament is None:
+        tournament = robostat.default_tournament
+
+    with robostat.replace_default_tournament(tournament):
+        exec(open(init).read(), {})
+
+    app.tournament = tournament
+
+    if admin_password is None:
+        admin_password = app.config.get("ROBOSTAT_ADMIN_PASSWORD", None)
+
+    app.setup_login(admin_password)
+
+    db_url = db_url or os.environ.get(db_envvar, None) or app.config.get("ROBOSTAT_DB", None)
+
+    if db_url is None:
+        raise ValueError("No database url given")
+
+    app.configure_db(db_url)
+
+    if "ROBOSTAT_LOGFILE" in app.config:
+        import logging
+        from robostat.web.logging import logger
+        handler = logging.FileHandler(app.config["ROBOSTAT_LOGFILE"])
+        handler.setLevel(logging.INFO)
+        handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(name)s %(message)s"))
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+
+    from robostat.web.views.judging import JudgingView
+    from robostat.web.views.ranking import RankingView
+    from robostat.web.views.timetable import TimetableView
+    from robostat.web.views.api import ApiView
+
+    judging = JudgingView()
+    ranking = RankingView()
+    timetable = TimetableView()
+    api = ApiView()
+
+    app.register_blueprint(judging, url_prefix="/judging")
+    app.register_blueprint(ranking, url_prefix="/ranking")
+    app.register_blueprint(timetable, url_prefix="/timetable")
+    app.register_blueprint(api, url_prefix="/api")
+
+    if admin_password is not None:
+        from robostat.web.views.admin import AdminView
+        admin = AdminView(
+                api_blueprint=api,
+                judging_blueprint=judging,
+                ranking_blueprint=ranking
+        )
+
+        app.register_blueprint(admin, url_prefix="/admin")
+
+    return app
