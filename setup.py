0a3e0e0aa772458dbca9f9c59aec576d487fb371
tl <tl@cat.pm>
2019-04-18 02:16:18 +0300

    setup: github linkit

--- setup.py
+++ setup.py
@@ -7,11 +7,14 @@ setup(
             "robostat.web",
         ],
         install_requires = [
-            "robostat3-core",
+            "robostat3-core @ https://github.com/teknologiakerho/robostat-core/tarball/master",
             "sqlalchemy",
             "flask"
         ],
         extras_require = {
-            "dev": ["pytest", "pttt"]
+            "dev": [
+                "pytest",
+                "pttt @ https://github.com/vfprintf/pttt/tarball/master"
+            ]
         }
 )
